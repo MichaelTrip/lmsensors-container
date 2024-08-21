22f923db379c610419a39e774a10ccdb0d57585d
Michael Trip <michael@alcatrash.org>
2024-08-21 10:54:36 +0200

    removing empty lines at the end of the fastfetch output

--- sensors.sh
+++ sensors.sh
@@ -4,8 +4,8 @@ while true; do
     echo "refreshed @ $(date) \n" > /data/lmsensors-$NODE_NAME.txt
     sensors >> /data/lmsensors-$NODE_NAME.txt
     echo "refreshed @ $(date) \n \n" > /data/fastfetch-$NODE_NAME.txt
-    fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g'
-    fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g' >> /data/fastfetch-$NODE_NAME.txt
+    fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g' | sed ':a;/^\s*$/{$d;N;/\S/!ba}'
+    fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g' | sed ':a;/^\s*$/{$d;N;/\S/!ba}' >> /data/fastfetch-$NODE_NAME.txt
     echo $?
     sleep 60
 done
