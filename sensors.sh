20320f9c719f8cea1d9fd6cf1d307c4d99123c15
Michael Trip <michael@alcatrash.org>
2024-08-21 08:31:58 +0200

    debugging

--- sensors.sh
+++ sensors.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 while true; do
+    set -x
     sensors > /data/lmsensors-$NODE_NAME.txt
-    fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g' > fastfetch-$NODE_NAME.txt
+    fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g'
     sleep 60
 done
