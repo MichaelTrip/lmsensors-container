b1dc41f1757977e3dc3358e0f9258b8e35439eb1
Michael Trip <michael@alcatrash.org>
2024-08-21 09:51:36 +0200

    updated script

--- sensors.sh
+++ sensors.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 while true; do
     set -x
-    sensors > /data/lmsensors-$NODE_NAME.txt
+    echo "refreshed @ $(date) \n" > /data/lmsensors-$NODE_NAME.txt
+    sensors >> /data/lmsensors-$NODE_NAME.txt
+    echo "refreshed @ $(date) \n \n" > /data/fastfetch-$NODE_NAME.txt
     fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g'
-    fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g' > /data/fastfetch-$NODE_NAME.txt
+    fastfetch --pipe --logo none | sed 's/\x1b\[[0-9;]*m//g' >> /data/fastfetch-$NODE_NAME.txt
     echo $?
     sleep 60
 done
